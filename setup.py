aa58e2157fcf78a37fca09a7f8700d31759b5bfc
Ehsan KhademOlama <ekoshv.igt@gmail.com>
2023-02-09 18:40:59 +0100

    confirmed

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ekoptim",
+    version="1.0.0",
+    packages=["ekoptim"],
+    url="https://github.com/ekoshv/Markowitz_Optim",
+    project_urls={
+        "LinkedIn": "https://www.linkedin.com/in/ehsan-khademolama-05149865/",
+    },
+    license="Licensed",
+    author="@EhsanKhademOlama",
+    author_email="ekoshv.igt@gmail.com",
+    description="Markowitz Optimizer",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    install_requires=[
+        "setuptools",
+        "pandas",
+        "scipy",
+        "sklearn"
+    ],
+)
