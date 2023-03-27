56067bdcdd8f34262866c23f992689f0a70b6d9c
Ehsan KhademOlama <ekoshv.igt@gmail.com>
2023-03-28 01:02:30 +0200

    confirmed

--- ekoptim/main.py
+++ ekoptim/main.py
@@ -321,14 +321,16 @@ class ekoptim():
         for i in range(self.Dyp, len(df)-self.Dyf+1, self.Thi):
             past_data = df[smb_col].iloc[i-self.Dyp:i]
             past_data_normalized, mindf, maxdf = self.normalize(past_data)
-            past_data_normalized_rs = np.array(past_data_normalized).reshape((n, m))
+            past_data_normalized_w = self.decompose_and_flatten(past_data_normalized,'db1')
+            past_data_normalized_w_rs = np.array(past_data_normalized_w).reshape((n, m))
+            past_data_normalized_w_rs_tl = np.tile(past_data_normalized_w_rs, (2,2))
             future_data = df[smb_col].iloc[i:i+self.Dyf]
             future_data_rescaled = ((future_data - mindf) /
                                     (maxdf - mindf))
             flattened_coeffs, lengths = self.decompose_and_flatten(future_data_rescaled.values, 'db1')
             flattened_coeffs[1:] = [num*spn for num in flattened_coeffs[1:]]
             new_row = {
-                'past_data': past_data_normalized_rs,
+                'past_data': past_data_normalized_w_rs_tl,
                 'future_data': flattened_coeffs,
                 'cLength': lengths,
                 'minmax': [mindf,maxdf]
@@ -349,7 +351,7 @@ class ekoptim():
     def model_create(self):
         n, m = self.reshape_nm(self.Dyp)
         model = tf.keras.Sequential([
-            tf.keras.layers.Input(shape=(n, m, 1)),
+            tf.keras.layers.Input(shape=(2*n, 2*m, 1)),
             
             tf.keras.layers.Conv2D(filters=max(round(self.Dyp/4), 32),
                            kernel_size=9, strides=1,
@@ -458,9 +460,11 @@ class ekoptim():
         # Normalize the past data using the same min and max values used during training
         past_data_normalized, mindf, maxdf = self.normalize(past_data)
         n, m = self.reshape_nm(self.Dyp)
-        past_data_normalized_rs = np.array(past_data_normalized).reshape((n, m))
+        past_data_normalized_w = self.decompose_and_flatten(past_data_normalized,'db1')
+        past_data_normalized_w_rs = np.array(past_data_normalized_w).reshape((n, m))
+        past_data_normalized_w_rs_tl = np.tile(past_data_normalized_w_rs, (2,2))
         # Reshape the past data for input to the neural network
-        X = np.expand_dims(past_data_normalized_rs, axis=(0, -1))
+        X = np.expand_dims(past_data_normalized_w_rs_tl, axis=(0, -1))
         # Use the trained neural network model to predict the future data
         y_pred = np.array(self.nnmodel.predict(X))
         y_pred = y_pred.squeeze()
