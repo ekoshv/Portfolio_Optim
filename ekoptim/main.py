68fa46e5f1fd047326bf9f9194eeb68c00399c5d
Ehsan KhademOlama <ekoshv.igt@gmail.com>
2023-05-03 11:26:50 +0200

    Update main.py

--- ekoptim/main.py
+++ ekoptim/main.py
@@ -434,15 +434,23 @@ class ekoptim():
         self.Dyf = Dyf   # Number of future days to predict in the moving horizon
         self.Thi = Thi   # Time horizon interval (in days)
         self.tile_size = tile_size
-        self.HNrates = []
+        
         if Selected_symbols is None:
             for df in self.full_rates:
+                # Find the index of the 'close' column
+                close_idx = df.columns.get_loc('close')
                 df['state']=-1
+                df.insert(close_idx + 1, 'state', df.pop('state'))
+                
             self.HNrates = self.Hrz_Nrm(self.full_rates, symb, spn, tile_size, xrnd)
         else:
             self.selected_rates = [df for df in self.full_rates if df.columns[-1] in Selected_symbols]
             for df in self.selected_rates:
+                # Find the index of the 'close' column
+                close_idx = df.columns.get_loc('close')
                 df['state']=-1
+                df.insert(close_idx + 1, 'state', df.pop('state'))
+                
             self.HNrates = self.Hrz_Nrm(self.selected_rates, symb, spn, tile_size, xrnd)
         self.mz = self.HNrates[0][0]['past_data'].shape[0]
         self.nz = self.HNrates[0][0]['past_data'].shape[1]
