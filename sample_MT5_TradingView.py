2ecc0232bcf5a4c9dca0df1f6f149fae60d7fd5a
Ehsan KhademOlama <ekoshv.igt@gmail.com>
2023-05-27 07:54:19 +0200

    confirmed

--- sample_MT5_TradingView.py
+++ sample_MT5_TradingView.py
@@ -252,7 +252,7 @@ if __name__ == "__main__":
                              tile_size=(n_t,int(n_t*Dqp/4)),xrnd=1e-3,#(n*Dqp->m=n*Dqp/4)
                              Selected_symbols=selected_symb,
                              Dqp=Dqp, Dyp=Dyp, Dyf=Dyf, Thi=1) #None
-    alphax = optimizerTV.HNrates[1]
+    alphax = optimizerTV.HNrates[1][100]
     cetax = optimizerTV.selected_rates
     optimizerTV.draw_states(cetax[-2])
 #%%
